d6896879f15a651a348e77bfd66cc031f5dc2bd8
Ghosuto <clash.raja10@gmail.com>
2026-07-05 11:28:48 +0000

    services: Add ambient display action [1/3]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2430,6 +2430,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case RINGER_MODES:
                 toggleRingerModes();
                 break;
+            case AMBIENT_DISPLAY:
+                triggerAmbientDisplay();
+                break;
             default:
                 break;
         }
@@ -7634,6 +7637,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
         return mRearFlashCameraId;
+    }
+
+    private void triggerAmbientDisplay() {
+        if (DEBUG_INPUT) {
+            Slog.d(TAG, "triggerAmbientDisplay");
+        }
+        Intent intent = new Intent("com.android.systemui.doze.pulse");
+        intent.setPackage("com.android.systemui");
+        mContext.sendBroadcastAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
     }
 
     private class TorchModeCallback extends CameraManager.TorchCallback {
